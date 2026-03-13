680024d485ed2cab4bc0e6e71c0d9c60e034e3ad
Andrew Woods <drew.woods@gmail.com>
2026-03-13 05:43:09 +0100

    emscripten: added gl4es_bootstrap.c

---
+++ packaging/web/gl4es_bootstrap.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Forward declaration of a initialization function
+void initialize_gl4es(void);
+
+// This attribute tells the linker to execute this function
+// before main() is called.
+__attribute__((constructor)) void gl4es_bootstrap(void) {
+    printf("[gl4es_bootstrap] Initializing gl4es configuration...\n");
+
+    // --- GL4ES Configuration ---
+    // These behave just like the environment variables you might set in a shell.
+
+    // 1. Enable Non-Power-Of-Two texture support (Crucial for UI/Fonts)
+    setenv("LIBGL_NPOT", "1", 1);
+
+    // 2. Force ES 2.0 backend (since we are compiling for WebGL 2)
+    setenv("LIBGL_ES", "2", 1);
+
+    // 3. (Optional) Show gl4es debug info in the browser console
+    setenv("LIBGL_DEBUG", "1", 1);
+
+    initialize_gl4es();
+
+    printf("[gl4es_bootstrap] Done.\n");
+}
