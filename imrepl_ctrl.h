c3a36418fc06b5ccfffc41bc2f97733316677b27
Andrew Woods <drew.woods@gmail.com>
2026-04-27 15:40:47 +0200

    Extract frame controller wrappers

---
+++ imrepl_ctrl.h
@@ -0,0 +1,8 @@
+#ifndef IMREPL_CTRL_H
+#define IMREPL_CTRL_H
+
+void imrepl_ctrl_init_gl(void);
+void imrepl_ctrl_display_frame(void);
+void imrepl_ctrl_reshape(int w, int h);
+
+#endif /* IMREPL_CTRL_H */
