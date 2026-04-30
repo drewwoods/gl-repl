b4d244131bb11d685bd88b529032f382dff70df7
Andrew Woods <drew.woods@gmail.com>
2026-04-30 22:55:18 +0200

    refactor: drop redundant typedefs, add repl_debug_dump_runtime_state_layout

--- repl_state.h
+++ repl_state.h
@@ -1,81 +1,37 @@
 #ifndef REPL_STATE_H
 #define REPL_STATE_H
 
-#include "repl_eval.h"
 #include "repl_state_views.h"
 #include "repl_state_owners.h"
 
-typedef ReplDocumentState ReplDocumentRuntimeState;
-
-typedef ReplFlatProgramState ReplFlatProgramRuntimeState;
-
-typedef ReplVariableState ReplVariableRuntimeState;
-
-typedef ReplEditorInputState ReplEditorInputRuntimeState;
-
-typedef ReplSelectionState ReplSelectionRuntimeState;
-
-typedef ReplClipboardState ReplClipboardRuntimeState;
-
-typedef ReplCodePanelRuntimeState ReplCodePanelRuntimeStorage;
-
-typedef ReplHelpState ReplHelpRuntimeState;
-
-typedef ReplVariablePanelState ReplVariablePanelRuntimeState;
-
-typedef ReplVariableDragState ReplVariableDragRuntimeState;
-
-typedef ReplProfilePanelState ReplProfilePanelRuntimeState;
-
-typedef ReplStatusState ReplStatusRuntimeState;
-
-typedef ReplSearchState ReplSearchRuntimeState;
-
-typedef ReplAutocompleteState ReplAutocompleteRuntimeState;
-
-typedef ReplCameraState ReplCameraRuntimeState;
-
-typedef ReplPointerState ReplPointerRuntimeState;
-
-typedef ReplViewportState ReplViewportRuntimeState;
-
-typedef ReplPresentationState ReplPresentationRuntimeState;
-
-typedef ReplRenderState ReplRenderRuntimeState;
-
-typedef ReplReplayRuntimeState ReplReplayRuntimeStateStore;
-
-typedef ReplSceneRuntimeState ReplSceneRuntimeStateStore;
-
-typedef ReplImportExportState ReplImportExportRuntimeState;
-
 typedef struct {
-    ReplDocumentRuntimeState         document;
-    ReplFlatProgramRuntimeState      flat_program;
-    ReplVariableRuntimeState         variables;
-    ReplEditorInputRuntimeState      editor_input;
-    ReplSelectionRuntimeState        selection;
-    ReplClipboardRuntimeState        clipboard;
-    ReplCodePanelRuntimeStorage      code_panel;
-    ReplHelpRuntimeState             help;
-    ReplVariablePanelRuntimeState    variable_panel;
-    ReplVariableDragRuntimeState     variable_drag;
-    ReplProfilePanelRuntimeState     profile_panel;
-    ReplStatusRuntimeState           status;
-    ReplSearchRuntimeState           search;
-    ReplAutocompleteRuntimeState     autocomplete;
-    ReplCameraRuntimeState           camera;
-    ReplPointerRuntimeState          pointer;
-    ReplViewportRuntimeState         viewport;
-    ReplPresentationRuntimeState     presentation;
-    ReplRenderRuntimeState           render;
-    ReplReplayRuntimeStateStore      replay;
-    ReplSceneRuntimeStateStore       scenes;
-    ReplImportExportRuntimeState     import_export;
+    ReplDocumentState         document;
+    ReplFlatProgramState      flat_program;
+    ReplVariableState         variables;
+    ReplEditorInputState      editor_input;
+    ReplSelectionState        selection;
+    ReplClipboardState        clipboard;
+    ReplCodePanelRuntimeState code_panel;
+    ReplHelpState             help;
+    ReplVariablePanelState    variable_panel;
+    ReplVariableDragState     variable_drag;
+    ReplProfilePanelState     profile_panel;
+    ReplStatusState           status;
+    ReplSearchState           search;
+    ReplAutocompleteState     autocomplete;
+    ReplCameraState           camera;
+    ReplPointerState          pointer;
+    ReplViewportState         viewport;
+    ReplPresentationState     presentation;
+    ReplRenderState           render;
+    ReplReplayRuntimeState    replay;
+    ReplSceneRuntimeState     scenes;
+    ReplImportExportState     import_export;
 } ReplRuntimeState;
 
 void repl_state_capture(ReplRuntimeState *snapshot);
 void repl_state_restore(const ReplRuntimeState *snapshot);
+void repl_state_dump_layout(void);
 
 
 #endif /* REPL_STATE_H */
