50807737458df2c4585338fbfb6463b4e13f7581
Andrew Woods <drew.woods@gmail.com>
2026-07-14 16:17:01 +0200

    build: make web-serve target dynamically find an available port starting from 8000

---
+++ scripts/web-serve.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys
+import os
+import http.server
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: web-serve.py <directory>", file=sys.stderr)
+        sys.exit(1)
+        
+    directory = sys.argv[1]
+    os.chdir(directory)
+    
+    port = 8000
+    while True:
+        try:
+            server = http.server.HTTPServer(('', port), http.server.SimpleHTTPRequestHandler)
+            print(f"Serving HTTP on 0.0.0.0 port {port} (http://127.0.0.1:{port}/) ...")
+            try:
+                server.serve_forever()
+            except KeyboardInterrupt:
+                print("\nKeyboard interrupt received, exiting.")
+            break
+        except OSError:
+            port += 1
+
+if __name__ == "__main__":
+    main()
